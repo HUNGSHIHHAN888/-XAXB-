6b7ab3b0c59a28d56dd3592d18ee4b79444442b3
Student <Student@208-38>
2024-04-29 09:15:06 +0800

    加入專案檔案。

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace 視窗版本的XAXB_遊戲
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
